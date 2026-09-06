813d28a8c626387a756fd5f0ae1ecdecb15e7277
Alexander Litovkin <122627581+allitov@users.noreply.github.com>
2026-09-06 16:14:08 +0700

    подключил в проект junit и настроил логгер

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,31 @@
 plugins {
-    id("java")
+    application
+    alias(libs.plugins.lombok)
 }
 
 group = "io.allitov"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(25))
+    }
+}
+
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:6.0.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation(libs.log4j.slf4j)
+
+    testImplementation(platform(libs.junit.bom))
+    testImplementation(libs.junit.jupiter)
+    testRuntimeOnly(libs.junit.launcher)
+}
+
+tasks.run {
+    mainClass.set("io.allitov.mpt.Main")
 }
 
 tasks.test {
---
+++ src/main/java/io/allitov/mpt/Main.java
@@ -0,0 +1,11 @@
+package io.allitov.mpt;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+
+    static void main() {
+        log.info("Hello world!");
+    }
+}
