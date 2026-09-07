f6e5f041f856eb434c47597163093719f0b8ecf8
Alexander Litovkin <122627581+allitov@users.noreply.github.com>
2026-09-07 18:52:10 +0700

    написал тесты для второй и третьей функции

--- src/test/java/io/allitov/OrderCalcImplTest.java
+++ src/test/java/io/allitov/OrderCalcImplTest.java
@@ -19,8 +19,34 @@ class OrderCalcImplTest {
             "5000,   false, 4750.0",
             "0,      false, 0.0"
     })
-    void applyDiscount(double total, boolean isPremium, double expected) {
+    void shouldApplyDiscount(double total, boolean isPremium, double expected) {
         double actual = orderCalc.applyDiscount(total, isPremium);
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "-1, -1.0",
+            "5000, 0.0",
+            "4999, 300.0"
+    })
+    void shouldCalculateShipping(double total, double expected) {
+        double actual = orderCalc.calcShipping(total);
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "-100, 10, 20, -1",
+            "100, -10, 20, -1",
+            "100, 10, -20, -1",
+            "1000, 200, 50, 850",
+            "100, 200, 50, 0"
+    })
+    void shouldCalculateFinalPrice(double total, double discount, double shipping, double expected) {
+        double actual = orderCalc.finalPrice(total, discount, shipping);
 
         assertEquals(expected, actual);
     }
