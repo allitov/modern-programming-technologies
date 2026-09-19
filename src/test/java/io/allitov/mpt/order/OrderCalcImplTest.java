becde133a67f44818da7dfd84ebaf10555d3d974
Alexander Litovkin <122627581+allitov@users.noreply.github.com>
2026-09-19 19:13:30 +0700

    перенес первую лабу в отдельный пакет

--- src/test/java/io/allitov/mpt/order/OrderCalcImplTest.java
+++ src/test/java/io/allitov/mpt/order/OrderCalcImplTest.java
@@ -1,6 +1,5 @@
-package io.allitov;
+package io.allitov.mpt.order;
 
-import io.allitov.mpt.OrderCalcImpl;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
